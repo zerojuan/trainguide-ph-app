56b82c6100af4503e454514e6d18964ed34f8603
zerojuan <juliuscebreros@gmail.com>
2013-08-27 22:39:21 +0800

    Grunt and bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt){
 	}
 
 	try{
-		yeomanConfig.app = require('./components.json').appPath || yeomanConfig.app;
+		yeomanConfig.app = require('./bower.json').appPath || yeomanConfig.app;
 	}catch(e){}
 
 	grunt.initConfig({
@@ -37,8 +37,155 @@ module.exports = function(grunt){
 			},
 			all: [
 				'Gruntfile.js',
-				'<%= yeoman.app %>/scripts/{,&/}*.js'
+				'<%= yeoman.app %>/scripts/{,*/}*.js'
 			]
+		},
+		concat: {
+			dist: {
+				files: {
+					'<%= yeoman.dist %>/scripts/scripts.js': [
+						'.tmp/scripts/{,*/}*.js',
+						'<%= yeoman.app %>/scripts/{,*/}*.js'
+					]
+				}
+			}
+		},
+		rev: {
+			dist: {
+				files: {
+					src: [
+						'<%= yeoman.dist %>/scripts/{,*/}*.js',
+						'<%= yeoman.dist %>/stylesheets/{,*/}*.css',
+						'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+						'<%= yeoman.dist %>/stylesheets/fonts/*'
+					]
+				}
+			}
+		},
+		useminPrepare: {
+			html: '<%= yeoman.app %>/index.html',
+			options: {
+				dest: '<%= yeoman.dist %>'
+			}
+		},
+		usemin: {
+			html: ['<%= yeoman.dist %>/{,*/}*.html'],
+			css: ['<%= yeoman.dist %>/stylesheets/{,*/}*.css'],
+			options: {
+				dirs : ['<%= yeoman.dist %>']
+			}
+		},
+		imagemin: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= yeoman.app %>/images',
+					src: '{,*/}*.{png, jpg, jpeg}',
+					dest: '<%= yeoman.dist %>/images'
+				}]
+			}
+		},
+		cssmin: {
+			// By default, your `index.html` <!-- Usemin Block --> will take care of
+			// minification. This option is pre-configured if you do not wish to use
+			// Usemin blocks.
+			dist: {
+			   files: {
+			     '<%= yeoman.dist %>/stylesheets/main.css': [
+			       '.tmp/stylesheets/{,*/}*.css',
+			       '<%= yeoman.app %>/stylesheets/{,*/}*.css'
+			     ]
+			   }
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					/*removeCommentsFromCDATA: true,
+					 // https://github.com/yeoman/grunt-usemin/issues/44
+					 //collapseWhitespace: true,
+					 collapseBooleanAttributes: true,
+					 removeAttributeQuotes: true,
+					 removeRedundantAttributes: true,
+					 useShortDoctype: true,
+					 removeEmptyAttributes: true,
+					 removeOptionalTags: true*/
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= yeoman.app %>',
+					src: '*.html',
+					dest: '<%= yeoman.dist %>'
+				}]
+			}
+		},
+		// Put files not handled in other tasks here
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					dot: true,
+					cwd: '<%= yeoman.app %>',
+					dest: '<%= yeoman.dist %>',
+					src: [
+						'*.{ico,png,txt}',
+						'.htaccess',
+						'components/**/*',
+						'images/{,*/}*.{gif,webp,svg}',
+						'stylesheets/fonts/*'
+					]
+				}, {
+					expand: true,
+					cwd: '.tmp/images',
+					dest: '<%= yeoman.dist %>/images',
+					src: [
+						'generated/*'
+					]
+				}]
+			}
+		},
+		cdnify: {
+			dist: {
+				html: ['<%= yeoman.dist %>/*.html']
+			}
+		},
+		ngmin: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= yeoman.dist %>/scripts',
+					src: '*.js',
+					dest: '<%= yeoman.dist %>/scripts'
+				}]
+			}
+		},
+		uglify: {
+			dist: {
+				files: {
+					'<%= yeoman.dist %>/scripts/scripts.js': [
+						'<%= yeoman.dist %>/scripts/scripts.js'
+					]
+				}
+			}
 		}
-	})
+	});
+
+	grunt.registerTask('build', [
+		'clean:dist',
+		'useminPrepare',
+		'concat',
+		'copy',
+		'cdnify',
+		'ngmin',
+		'cssmin',
+		'uglify',
+		'rev',
+		'usemin'
+	]);
+
+	grunt.registerTask('default', [
+		'jshint',
+		'test',
+		'build'
+	])
 }
