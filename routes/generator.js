d3869ecbd5795f9e88f3ebd8a155b2bf00002ccf
zerojuan <juliuscebreros@gmail.com>
2013-08-28 00:13:49 +0800

    Added insertion of transfers in static generator

--- routes/generator.js
+++ routes/generator.js
@@ -1,10 +1,10 @@
 
 var constants = require('../constants'),
-		qs = require("querystring"),
+		qs = require('querystring'),
 	  gtfs = require('gtfs-2'),
-		request = require("request"),
+		request = require('request'),
 		async = require('async'),
-		fs = require("fs");
+		fs = require('fs');
 
 exports.generateStaticData = function(req, res, next){
 	var trainAgencies = constants.AGENCIES;
@@ -104,11 +104,40 @@ exports.generateStaticData = function(req, res, next){
 		getDirections(origin, dest, callback);
 	}
 
+	var searchStationByName = function(stationData){
+		var line = stationData.line;
+
+		for(var i = 0; i < line.stops.length; i++){
+			if(line.stops[i].details.stop_name == stationData.name){
+				return line.stops[i];
+			}
+		}
+	}
+
+	var insertTransfer = function(transfer){
+		var stationA, stationB;
+
+		stationA = searchStationByName(transfer[0]);
+		stationB = searchStationByName(transfer[1]);
+
+		stationA.transfer = {
+			name: stationB.details.stop_name,
+			_id: stationB._id
+		};
+
+		stationB.transfer = {
+			name: stationA.details.stop_name,
+			_id: stationA._id
+		};
+	}
+
 	//get route from each agency
 	async.series([
 		function(callback){
 			findStops(constants.ROUTE_IDS.LRT1, function(result){
 				trainLines.LRT1 = result;
+
+				//TODO: insert additional data here
 
 				getPath(trainLines.LRT1, function(path){
 					trainLines.LRT1.path = path;
@@ -120,6 +149,8 @@ exports.generateStaticData = function(req, res, next){
 			findStops(constants.ROUTE_IDS.LRT2, function(result){
 				trainLines.LRT2 = result;
 
+				//TODO: insert additional data here
+
 				getPath(trainLines.LRT2, function(path){
 					trainLines.LRT2.path = path;
 					callback();
@@ -130,6 +161,8 @@ exports.generateStaticData = function(req, res, next){
 			findStops(constants.ROUTE_IDS.MRT, function(result){
 				trainLines.MRT = result;
 
+				//TODO: insert additional data here
+
 				getPath(trainLines.MRT, function(path){
 					trainLines.MRT.path = path;
 					callback();
@@ -139,6 +172,8 @@ exports.generateStaticData = function(req, res, next){
 		function(callback){
 			findStops(constants.ROUTE_IDS.PNR, function(result){
 				trainLines.PNR = result;
+
+				//TODO: insert additional data here
 
 				getPath(trainLines.PNR, function(path){
 						trainLines.PNR.path = path;
@@ -153,6 +188,22 @@ exports.generateStaticData = function(req, res, next){
 
 		var outputFilename = './public/data/lines.data.json';
 
+		//insert the transfers here
+		var transfers = [
+			[{name: 'EDSA PNR', line: trainLines.PNR}, {name: 'Magallanes MRT', line: trainLines.MRT}],
+			[{name: 'Santa Mesa PNR', line: trainLines.PNR}, {name: 'Pureza LRT', line: trainLines.LRT2}],
+			[{name: 'Blumentritt PNR', line: trainLines.PNR}, {name: 'Blumentritt LRT', line: trainLines.LRT2}],
+			[{name: 'Doroteo Jose LRT', line: trainLines.LRT1}, {name: 'Recto LRT', line: trainLines.LRT2}],
+			[{name: 'EDSA LRT', line: trainLines.LRT1}, {name: 'Taft MRT', line: trainLines.MRT}],
+			[{name: 'Cubao LRT', line: trainLines.LRT2}, {name: 'Cubao MRT', line: trainLines.MRT}]
+		];
+
+		//insert the transfers
+		for(var i in transfers){
+			var transfer = transfers[i];
+			insertTransfer(transfer);
+		}
+
 		//get shape data from each array of stops
 
 		fs.writeFile(outputFilename, JSON.stringify(trainLines, null, 4), function(err){
@@ -166,8 +217,6 @@ exports.generateStaticData = function(req, res, next){
 				});
 			}
 		});
-
-		//construct a json file out of it
 	});
 
 }
