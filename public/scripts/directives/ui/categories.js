3addeec623c4964db5dd35215284dd910aff1a1b
janejanejane <jeanclaudetteambait@gmail.com>
2013-08-29 09:58:33 +0800

    Fix sidebar layout

--- public/scripts/directives/ui/categories.js
+++ public/scripts/directives/ui/categories.js
@@ -23,13 +23,13 @@ angular.module('uiModule').directive('categories', function(){
       '<div class="categories-list" ng-transclude>'+
         '<div>'+
           '<ul>'+
-            '<li ng-show="category.icon" ng-repeat="category in categories">'+
-              '<i class="{{category.icon}}" ng-click="setCategory(category.name)"></i>'+
-              '<div ng-show="selectedCategory==category.name" class="down-btn"></div>'+
+            '<li ng-show="category.icon" ng-repeat="category in categories" ng-class="{\'selected\': selectedCategory==category.name}">'+
+              '<i class="{{category.icon}}" ng-click="setCategory(category.name)" ng-class="{\'selected\': selectedCategory==category.name}"></i>'+
+              '<div ng-show="selectedCategory==category.name" class="highlight"></div>'+
             '</li>'+
           '</ul>'+
         '</div>'+
-        '<h6>{{selectedCategory}}</h6>'+
+        '<h6>Featured</h6>'+
       '</div>',
     replace : true
   }
