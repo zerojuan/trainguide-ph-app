5fe1638a0ad4436be691cad40b289a5f31d288e6
janejanejane <jeanclaudetteambait@gmail.com>
2013-09-19 17:53:19 +0800

    Add direction step details on click: include jeep, bus

--- public/scripts/directives/ui/direction.js
+++ public/scripts/directives/ui/direction.js
@@ -14,8 +14,8 @@ angular.module('uiModule').directive('direction', ['$filter', function($filter){
       scope.selectedStep = null;
       scope.divClass = 'align';
       scope.trueMode = $filter('realmode')(scope.leg.mode, scope.leg.routeId);
-      scope.showMe = (scope.trueMode == 'WALK' || scope.trueMode == 'RAIL');      
-      console.log('leg route', scope.leg.route);
+      scope.showMe = (scope.trueMode == 'WALK' || scope.trueMode == 'RAIL');  
+
       if(scope.leg.route){
         switch(scope.leg.route){
           case 'LRT 1':
@@ -32,6 +32,7 @@ angular.module('uiModule').directive('direction', ['$filter', function($filter){
             break;
         }
       }
+
       if(!scope.isLast()){
         scope.divClass = scope.trueMode;
         if(scope.trueMode == 'RAIL')
@@ -45,23 +46,24 @@ angular.module('uiModule').directive('direction', ['$filter', function($filter){
           scope.selectedStep = null;
         }
       };
-
-      console.log('scope.trueMode', scope.trueMode, 'scope.showMe', scope.showMe, 'scope.isLast', scope.isLast(), 'scope.divClass', scope.divClass);
     },
     template :
       '<div>'+
-        '<div class="{{divClass}}">'+
+        '<div class="{{divClass}}" ng-class="{isparent: trueMode!=\'RAIL\'}" ng-click="clickedDirection(leg)">'+
           '<div class="{{trueMode}} circle {{routeCode}}"></div>'+
           '<p>{{trueMode}}</p>'+
           '<p ng-hide="showMe"><em>{{leg.route}}</em></p>'+
-          '<p ng-show="showMe" ng-class="{isparent: leg.steps.length>0}" ng-click="clickedDirection(leg)">'+
+          '<p ng-show="showMe">'+
             '<em>{{leg.from.name}}</em> to <em>{{leg.to.name}}</em>'+
           '</p>'+
           '<ul ng-show="leg.steps.length && selectedStep==leg" class="direction-steps">'+
             '<li ng-repeat="step in leg.steps">'+
-              '<span>{{step.relativeDirection}} on {{step.streetName}}</span>'+
+              '<span>{{step.relativeDirection|parseDirection}} on {{step.streetName}}</span>'+
             '</li>'+
           '</ul>'+
+          '<p ng-show="trueMode!=\'RAIL\' && (leg.steps.length==0 && selectedStep==leg)" class="direction-steps">'+
+            '<span><em>{{leg.from.name}}</em> to <em>{{leg.to.name}}</em></span>'+
+          '</p>'+
         '</div>'+
       '</div>',
     replace : true
