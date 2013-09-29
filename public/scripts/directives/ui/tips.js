d23a148d1fbc7678cc07908a7604e3f05e22b2a7
janejanejane <jeanclaudetteambait@gmail.com>
2013-09-29 14:01:28 +0800

    Update trips, places, download css

--- public/scripts/directives/ui/tips.js
+++ public/scripts/directives/ui/tips.js
@@ -21,6 +21,7 @@ angular.module('uiModule').directive('tips', function(){
         '<ul>'+
           '<li ng-repeat="tip in tips" ng-click="clickedTip(tip.title)">'+
             '<h6>{{tip.title}}</h6>'+
+            '<span class="down-btn"></span>'+
             '<div ng-show="tip.selected" ng-hide="tip.selected==false">'+
               '<img src="{{tip.image}}" />'+
               '<p ng-repeat="detail in tip.details">{{detail}}</p>'+
