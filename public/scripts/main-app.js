a7089a32a1eb2a9dfdefdfff79740256fe81785a
zerojuan <juliuscebreros@gmail.com>
2013-11-07 17:56:37 +0800

    Sample for handling url routing

--- public/scripts/main-app.js
+++ public/scripts/main-app.js
@@ -6,12 +6,7 @@ angular.module("google-maps", ["trainguideServices"]);
 angular.module("trainguide.controllers", ["trainguideServices"]);
 
 
-angular.module("trainguide", ["google-maps", "trainguide.filters", "trainguide.controllers", "uiModule"])
-	.config(['$routeProvider', function($routeProvider){
-		$routeProvider
-			.when('/:state');
-
-	}]);
+angular.module("trainguide", ["google-maps", "trainguide.filters", "trainguide.controllers", "uiModule"]);
 
 angular.module("trainguide.controllers")
 	.controller("ViewerCtrl", ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
